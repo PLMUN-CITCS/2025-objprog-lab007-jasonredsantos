e88baf44255ce6807488c91746a7b493a172f385
jasonredsantos <santosjasonred_bscs@plmun.edu.ph>
2025-03-03 16:01:39 +0800

    "Submitting OBJPROG Week 04 - Session 01 - Exercise 01"

---
+++ BooleanExpressionsDemo.java
@@ -0,0 +1,33 @@
+/*Is Java fun? true
+Is homework tiring? false
+Is 'a' greater than 'b'? false
+Are 'a' and 'b' equal? false
+Both conditions (isJavaFun && a<b): true
+Either condition (isJavaFun || isHomeworkTiring): true
+Logical NOT of isHomeworkTiring: true
+Exclusive OR (isJavaFun ^ isHomeworkTiring): true */
+
+
+public class BooleanExpressionsDemo {  
+      public static void main(String[] args){
+        boolean isJavaFun = true;
+        boolean isHomeworkTiring = false;  
+        int a = 15;
+        int b = 20;
+        boolean isAGreater = a > b;
+        boolean areEqual = a == b;
+        boolean bothTrue = isJavaFun && (a < b);
+        boolean eitherTrue = isJavaFun || isHomeworkTiring;
+        boolean notTrue =!isHomeworkTiring;
+        boolean exclusiveOr = isJavaFun ^ isHomeworkTiring;
+        System.out.println("Is Java fun? " + isJavaFun);
+        System.out.println("Is homework tiring? " + isHomeworkTiring);
+        System.out.println("Is 'a' greater than 'b'? " + isAGreater);
+        System.out.println("Are 'a' and 'b' equal? " + areEqual);
+        System.out.println("Both conditions (isJavaFun && a<b): " + bothTrue);
+        System.out.println("Either condition (isJavaFun || isHomeworkTiring): " + eitherTrue);
+        System.out.println("Logical NOT of isHomeworkTiring: " + notTrue);
+        System.out.println("Exclusive OR (isJavaFun ^ isHomeworkTiring): " + exclusiveOr);
+    }
+
+}
